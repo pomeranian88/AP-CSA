11d994acdfa3ab05aefede9f1d2867966abbb4f6
zev <51273353+pomeranian88@users.noreply.github.com>
2023-09-11 10:19:51 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,22 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        int score;
+        score = 0;
+        System.out.print("The score is ");
+        System.out.println(score);
+
+        double price = 23.25;
+        System.out.println("The price is " + price);
+
+        boolean won = false;
+        System.out.println(won);
+        won = true;
+        System.out.println(won);
+
+        String name = "Jose";
+        System.out.println("Hi " + name);
+    }
+}
+
