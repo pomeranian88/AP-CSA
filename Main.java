29b69a00d0a601ff3118217038b1e9522a683608
zev <51273353+pomeranian88@users.noreply.github.com>
2023-09-11 16:37:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
-class Main{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello, world!");
-    System.out.print("now this is insane");
-  }
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int score = 2;
+        System.out.print("The score is ");
+        System.out.println(score);
+
+        final double price = 23.25;
+        System.out.println("The price is " + price);
+
+        boolean won = false;
+        System.out.println(won);
+        won = true;
+        System.out.println(won);
+
+        final String name = "Jose";
+        System.out.println("Hi " + name);
+    }
 }
