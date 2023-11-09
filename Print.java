e4b542d854bd68611b0ed7a9a0a5ae75052c4cd3
zev <51273353+pomeranian88@users.noreply.github.com>
2023-11-09 10:19:59 -0500

    Update Print.java
    this was big change. good job !

--- Print.java
+++ Print.java
@@ -1,49 +1,42 @@
 import java.util.Scanner;
 
-
 public class Print
 {
     private String name;
-    private int value;
-    private boolean yon;
     Calculate clack = new Calculate();
     Scanner s = new Scanner(System.in);
-
 
     public void getName(){
         System.out.println("What's your name? ");
         name = s.next();
     }
     public int welcome(){
-        System.out.println("-------------------------");
+        System.out.println("Welcome to the Pythagorean Calculator!\n-------------------------");
         System.out.println("Would you like to\n1) Calculate the hypotenuse of a triangle\n2) Calculate the slope between two points");
         System.out.println("Enter 1 or 2: ");
         return s.nextInt();
     }
     public void printTri() {
         clack = new Calculate(name);
-        clack.makeSideOne();
-        clack.makeSideTwo();
-        double hyp = clack.getHyp(); // calculate hypotenuse length
-        value = (int) hyp; // cast hyp into an int to truncate it
-        yon = (hyp - value) == 0.0; // if subtracting the double - the truncated yields 0.0, it's a pythag triple
-        System.out.println("The hypotenuse of this triangle is: " + hyp);
-        System.out.println("This triangle is a pythagorean triple: " + yon); // 2.6 PT.3
+        clack.getSides();
+        clack.makeHyp();
+        System.out.println("The hypotenuse of this triangle is: " + clack.getHyp());
+        System.out.println("This triangle is a pythagorean triple: " + clack.getTripleOrNot()); // 2.6 PT.3
         System.out.println();
 
-
         clack.Increase(name);
-        value = (int)clack.getHyp();
-        yon = (clack.getHyp() - value) == 0.0; // redo all these calculation
+        clack.makeHyp();
         System.out.println("After increasing side1 by that amount and decreasing side2 by that amount...");
         System.out.println("The hypotenuse of this triangle is: " + clack.getHyp());
-        System.out.println("This triangle is a pythagorean triple: " + yon);
+        System.out.println("This triangle is a pythagorean triple: " + clack.getTripleOrNot());
     }
     public void printSlope() {
         Slope kack = new Slope();
-        System.out.println("-------------------------");
         kack.getPoints();
         kack.calculateSlope();
+        System.out.println("The distance between these points = " + kack.getHyp());
+        System.out.println("The y intercept of this line is (0, " + kack.getYInt() + ")");
+        System.out.println("The equation of this line is: y = " + kack.getSlope() + "x + " + kack.getYInt());
     }
     public void dothething(){
         System.out.println(clack);
