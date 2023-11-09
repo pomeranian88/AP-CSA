fd4a9dfa3371a59c26e3a69d7f1fc9643c85a564
zev <51273353+pomeranian88@users.noreply.github.com>
2023-11-09 10:19:27 -0500

    Update Calculate.java
    this was big change. good job !

--- Calculate.java
+++ Calculate.java
@@ -1,53 +1,45 @@
 import java.util.Scanner;
 
-
 public class Calculate
 {
     private int side1;
     private int side2;
     private double hyp;
     private final String name;
-
+    private boolean tripleOrNot;
 
     public Calculate(){
         name = "User";
     }
 
-
     public Calculate(String n){
         name = n;
     }
 
-
-    public void makeSideOne() {
-        System.out.println("-------------------------\nWelcome to the Pythagorean calculator, " + name + "!\nEnter side 1:"); // 2.6 PT.2&4
+    public void getSides() {
         Scanner s = new Scanner(System.in);
-        side1 = s.nextInt(); // get side 1 length
-    }
-
-
-    public void makeSideTwo() {
+        System.out.println("Welcome to the Pythagorean calculator, " + name + "!\nEnter side 1:"); // 2.6 PT.2&4
+        side1 = s.nextInt();
         System.out.println("Enter side 2: ");
-        Scanner s = new Scanner(System.in);
         side2 = s.nextInt(); // get side 2 length
     }
 
-
     public void Increase(String n) {
-        System.out.println(n + ", how much do you want to increase side1 by?: ");
         Scanner s = new Scanner(System.in);
+        System.out.println(n + ", how much do you want to increase side1 by?: ");
         int increase = s.nextInt();
-        side1 += increase; // increase side1 by specified amount
+        side1 += increase;
         System.out.println("How much do you want to decrease side2 by?: ");
-        s = new Scanner(System.in);
         int decrease = s.nextInt();
         side2 -= decrease;
-        hyp = Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2));
     }
-    public double getHyp(){
+    public void makeHyp(){
         hyp = Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2));
-        return this.hyp;
+        int value = (int) hyp; // cast hyp into an int to truncate it
+        tripleOrNot = (hyp - value) == 0.0; // if subtracting the double - the truncated yields 0.0, it's a pythag triple
     }
+    public boolean getTripleOrNot(){return tripleOrNot;}
+    public double getHyp(){return hyp;}
     public String toString(){
         System.out.print("your final info: ");
         return "side1 = " + side1 + ", side2 = " + side2 + ", hyp = " + hyp + ", name = " + name;
