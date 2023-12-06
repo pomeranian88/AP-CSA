7bb41394d60111f98e6932b96df468be1b13f54b
zev <51273353+pomeranian88@users.noreply.github.com>
2023-12-06 10:05:52 -0500

    4.4 done lit af

--- Print.java
+++ Print.java
@@ -11,7 +11,7 @@ public class Print
         name = s.next();
     }
     public int welcome(){
-        System.out.println("Welcome to the Pythagorean Calculator!\n-------------------------");
+        System.out.println("Welcome to the Pythagorean Calculator, " + name + "!\n-------------------------");
         System.out.println("Would you like to\n1) Calculate the hypotenuse of a triangle\n2) Calculate the " +
                 "slope between two points\n3) Quit because you didn't mean to open this");
         System.out.println("Enter 1, 2, or 3: ");
@@ -39,6 +39,7 @@ public class Print
         System.out.println("The distance between these points = " + kack.getHyp());
         System.out.println("The y intercept of this line is (0, " + kack.getYInt() + ")");
         System.out.println("The equation of this line is: y = " + kack.getSlope() + "x + " + kack.getYInt());
+        kack.getMorePoints();
     }
     public void dothething(){
         System.out.println(clack);
